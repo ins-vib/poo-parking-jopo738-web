33ea5c6e10c27a5932536c1c1864539825f99f13
Joan Pomés <jopo738@vidalibarraquer.net>
2026-01-28 08:46:21 +0100

    primer commit

---
+++ src/main/java/org/yourcompany/parking/Parking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.parking;
+
+/**
+ *
+ * @author jpome
+ */
+public class Parking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
